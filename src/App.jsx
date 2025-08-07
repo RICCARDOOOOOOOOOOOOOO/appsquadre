f135a1708dda4f5a057718b6fbf86060f4207f55
gnaturani <guido.naturani@gmail.com>
2025-08-07 10:01:21 +0200

    fix login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useEffect } from 'react';
-import { isTokenValid } from "./services/auth";
+import { isTokenValid, setToken, clearToken } from './services/auth';
 import SquadGenerator from './Components/SquadGenerator'
+
+const LOGIN_URL =
+  'https://gestione.parrocchiacarpaneto.com/login/#/?returnUrl=' +
+  encodeURIComponent(
+    location.hostname === 'localhost'
+      ? 'http://localhost:5173'
+      : 'https://gestione.parrocchiacarpaneto.com/squadre',
+  );
 
 function App() {
   useEffect(() => {
     (async () => {
-      const valid = await isTokenValid();
-      if (!valid) {
-        window.location.href = 'https://gestione.parrocchiacarpaneto.com/login/?returnUrl=' + encodeURIComponent(window.location.href);
+      const qs = new URLSearchParams(location.search);
+      const tk = qs.get('token');
+      if (tk) {
+        setToken(tk);
+        qs.delete('token');
+        history.replaceState({}, '', location.pathname);
+      }
+      if (!(await isTokenValid())) {
+        clearToken();
+        location.href = LOGIN_URL;
       }
     })();
   }, []);
